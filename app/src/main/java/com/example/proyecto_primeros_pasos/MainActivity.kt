2c160b2d91ce604c8d82ae7e02d89d2a9480f598
benvarzam <benje1612@gmail.com>
2025-10-21 19:06:34 +0200

    Columna puesta

--- app/src/main/java/com/example/proyecto_primeros_pasos/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_primeros_pasos/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +32,13 @@ class MainActivity : ComponentActivity() {
         val modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
+        val resultado by remember { mutableStateOf(1) }
+        Column (
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
 
+        }
     }
 
 }
