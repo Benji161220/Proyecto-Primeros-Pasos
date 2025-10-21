b4cd4617d126fa788b61baf94838711dc6663215
benvarzam <benje1612@gmail.com>
2025-10-21 18:56:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/proyecto_primeros_pasos/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.proyecto_primeros_pasos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyecto_primeros_pasos.ui.theme.ProyectoPrimerosPasosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProyectoPrimerosPasosTheme {
+                ProyectoApp()
+            }
+        }
+    }
+    @Preview
+    @Composable
+    fun ProyectoApp(){
+        val modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+
+    }
+
+}
+
