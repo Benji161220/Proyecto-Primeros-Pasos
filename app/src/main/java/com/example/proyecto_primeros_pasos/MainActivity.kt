ef72ec107c3a78a93404560d4b5fd10cadce9e6f
benvarzam <benje1612@gmail.com>
2025-10-24 19:26:23 +0200

    array de tareas que se van a asignar

--- app/src/main/java/com/example/proyecto_primeros_pasos/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_primeros_pasos/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.proyecto_primeros_pasos.ui.theme.ProyectoPrimerosPasosTheme
 
@@ -29,6 +31,14 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun ProyectoApp(){
+        val tareasDAM = arrayOf(
+            "Implementar conexión con base de datos PostgreSQL en Kotlin",
+            "Diseñar interfaz gráfica en Android Studio con RecyclerView",
+            "Crear API REST con Spring Boot y probarla con Postman",
+            "Desarrollar módulo personalizado en Odoo con Python",
+            "Programar hilos para gestionar procesos en segundo plano",
+            "Realizar consulta SQL con JOINs en base de datos relacional",
+            "Diseñar diagrama UML del proyecto final")
         val modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
@@ -37,7 +47,7 @@ class MainActivity : ComponentActivity() {
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-
+            Text(stringResource(R.string.explicacionAPP))
         }
     }
 
